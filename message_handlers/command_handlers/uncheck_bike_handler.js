6c750cb6f831307d00d05d61383630ea9586c4f2
bnopne <bnopne@gmail.com>
2017-07-20 17:48:27 +0300

    Fix uncheck bike handler.

--- message_handlers/command_handlers/uncheck_bike_handler.js
+++ message_handlers/command_handlers/uncheck_bike_handler.js
@@ -13,14 +13,14 @@ class UncheckBikeCommandHandler extends BaseCommandHandler {
 
   _parseCommandArguments() {
     const result = {
-      chatId: this._message.getChat().getId(),
-      userId: null
+      chat: this._message.getChat(),
+      user: null
     };
 
     const reply = this._message.getReplyToMessage();
 
     if (reply) {
-      result.userId = reply.getSender().getId();
+      result.user = reply.getSender();
     };
 
     return result;
@@ -28,10 +28,10 @@ class UncheckBikeCommandHandler extends BaseCommandHandler {
 
   async handle() {
 
-    if (!this._commandArguments.userId) {
+    if (!this._commandArguments.user) {
 
       await this._bot.sendMessage(
-        this._commandArguments.chatId,
+        this._commandArguments.chat.getId(),
         'Ответь кому-нибудь, чтобы подтвердить, что он не показал свой велик, пук!'
       );
 
@@ -39,13 +39,18 @@ class UncheckBikeCommandHandler extends BaseCommandHandler {
 
     };
 
+    await this._db.makeSureChatAndUserExist(
+      this._commandArguments.chat,
+      this._commandArguments.user
+    );
+
     await this._db.chatsAndUsers.uncheckBike(
-      this._commandArguments.userId,
-      this._commandArguments.chatId
+      this._commandArguments.user.getId(),
+      this._commandArguments.chat.getId()
     );
 
     await this._bot.sendMessage(
-      this._commandArguments.chatId,
+      this._commandArguments.chat.getId(),
       'Теперь он беспруфный кукарек без велика, кек!'
     );
 
