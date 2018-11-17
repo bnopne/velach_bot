59eb4bc149bbc1061cf026911bbcd86039062e25
bnopne <bnopne@gmail.com>
2018-11-17 18:28:08 +0300

    Upgrade package version, remove token from settings

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -26,7 +26,7 @@ const settings = convict({
     token: {
       doc: 'Telegram bot token',
       format: String,
-      default: '342387136:AAGiuMdiRjsswdkj75-sF14bvF34ZdTmw1k',
+      default: '',
       env: 'TELEGRAM_TOKEN',
     },
     messageTTL: {
