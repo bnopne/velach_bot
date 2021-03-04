83b1c202b9cddbd6087f2b275499830ab7c7138f
bnopne <bnopne@gmail.com>
2021-03-04 23:06:41 +0300

    Fixup empty line when user has only one bikecheck

--- src/text/captions.js
+++ src/text/captions.js
@@ -1,15 +1,20 @@
-const getBikecheckCaption = (likes, dislikes, stravaLink, index = 0, totalBikechecks = 1, rank) => {
-  const rankText = rank === -1 ? '' : `Место _#${rank}_ в общем рейтинге\n`;
-  const postitionText = totalBikechecks > 1 ? `_Байкчек ${index + 1} из ${totalBikechecks}_` : '';
-  const stravaText = stravaLink ? `[Страва](${stravaLink})` : '_Страву не показывал_';
-  return `${rankText}${postitionText}\nНравится: ${likes}\nНе нравится: ${dislikes}\n${stravaText}`;
-};
+function getBikecheckCaption(likes, dislikes, stravaLink, index = 0, totalBikechecks = 1, rank) {
+  const lines = [
+    rank === -1 ? null : `Место _#${rank}_ в общем рейтинге`,
+    totalBikechecks > 1 ? `_Байкчек ${index + 1} из ${totalBikechecks}_` : null,
+    `Нравится: _${likes}_\nНе нравится: _${dislikes}_`,
+    stravaLink ? `[Страва](${stravaLink})` : '_Страву не показывал_',
+  ];
+  return lines.filter((line) => Boolean(line)).join('\n');
+}
 
-const getTopCaption = (position, likes, dislikes, user) => ([
-  `*Байкчек #${position}, владелец* [${user.firstName}](tg://user?id=${user.id})`,
-  `👍: _${likes}_ ÷ 👎: _${dislikes}_`,
-  user.stravaLink ? `[Страва](${user.stravaLink})` : '_Страву не показывал_',
-].join('\n'));
+function getTopCaption(position, likes, dislikes, user) {
+  return [
+    `*Байкчек #${position}, владелец* [${user.firstName}](tg://user?id=${user.id})`,
+    `👍: _${likes}_ ÷ 👎: _${dislikes}_`,
+    user.stravaLink ? `[Страва](${user.stravaLink})` : '_Страву не показывал_',
+  ].join('\n');
+}
 
 module.exports = {
   getBikecheckCaption,
