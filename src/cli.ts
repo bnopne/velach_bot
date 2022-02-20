9e545a8cfa1ac196b3c3aa3db6f95324d91367ad
bnopne <bnopne@gmail.com>
2022-02-20 21:39:48 +0300

    Add DB backup CLI command

--- src/cli.ts
+++ src/cli.ts
@@ -1,24 +1,21 @@
-import { promises as fs } from 'fs';
+import { readFileSync } from 'fs';
 import { join } from 'path';
-
-import { NestFactory } from '@nestjs/core';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { execSync } from 'child_process';
 
 import { createCommand } from 'commander';
 import { noop } from 'lodash';
+import { format } from 'date-fns';
+import { Dropbox } from 'dropbox';
 
 import { getConnection, disconnect } from 'src/common/database/connection';
 import { seedTestDatabase } from 'src/common/database/test-database';
+import {
+  getTestingModule,
+  getTestConfigService,
+} from 'src/common/utils/test-utils';
 
 interface ICliCommand {
   (): Promise<void>;
-}
-
-async function getConfigService(): Promise<ConfigService> {
-  const configModule = await NestFactory.create(
-    ConfigModule.forRoot({ envFilePath: join(process.cwd(), '.env') }),
-  );
-  return configModule.get(ConfigService);
 }
 
 async function execute(command: ICliCommand): Promise<void> {
@@ -34,10 +31,10 @@ async function noopCommand(): Promise<void> {
 }
 
 async function createTables(): Promise<void> {
-  const configService = await getConfigService();
+  const configService = await getTestConfigService(await getTestingModule([]));
   const connection = await getConnection(configService);
 
-  const script = await fs.readFile('./src/common/database/create-tables.sql');
+  const script = readFileSync('./src/common/database/create-tables.sql');
 
   try {
     await connection.query(script.toString());
@@ -50,7 +47,7 @@ async function createTables(): Promise<void> {
 }
 
 async function seedDatabase(): Promise<void> {
-  const configService = await getConfigService();
+  const configService = await getTestConfigService(await getTestingModule([]));
   const connection = await getConnection(configService);
 
   try {
@@ -63,12 +60,49 @@ async function seedDatabase(): Promise<void> {
   }
 }
 
+async function backupDatabase(): Promise<void> {
+  const configService = await getTestConfigService(await getTestingModule([]));
+
+  const dumpFilename = `velach_bot_database_dump_${format(
+    new Date(),
+    'dd.MM.yyyy_HH:mm:ss',
+  )}.sql`;
+  const dumpFullname = join('/tmp', dumpFilename);
+
+  execSync(
+    `pg_dump ${configService.get<string>(
+      'VELACH_BOT_DB_DATABASE',
+    )} > ${dumpFullname}`,
+    {
+      env: {
+        PGHOST: configService.get<string>('VELACH_BOT_DB_HOST'),
+        PGPORT: configService.get<string>('VELACH_BOT_DB_PORT'),
+        PGUSER: configService.get<string>('VELACH_BOT_DB_USER'),
+        PGPASSWORD: configService.get<string>('VELACH_BOT_DB_PASSWORD'),
+      },
+    },
+  );
+
+  const dropbox = new Dropbox({
+    fetch,
+    accessToken: configService.get<string>('VELACH_BOT_DROPBOX_TOKEN'),
+  });
+
+  const dump = readFileSync(dumpFullname);
+
+  await dropbox.filesUpload({
+    contents: dump,
+    path: `/${dumpFilename}`,
+  });
+}
+
 const program = createCommand();
 
 program
   .version('1.0.0')
-  .option('--create-tables', 'Syncronize ORM models to DB')
-  .option('--seed-test-db', 'Fills database with test data');
+  .option('--create-tables', 'Creates tables and corresponding stuff in DB')
+  .option('--seed-test-db', 'Fills DB with test data')
+  .option('--backup-db', 'Creates DB dump and uploads it to Dropbox');
 
 program.parse(process.argv);
 
@@ -80,6 +114,9 @@ if (program.opts().createTables) {
 } else if (program.opts().seedTestDb) {
   console.log('execute SEED TEST DATABASE');
   command = seedDatabase;
+} else if (program.opts().backupDb) {
+  console.log('execute BACKUP DATABASE');
+  command = backupDatabase;
 } else {
   console.warn('execute NO-OP');
 }
