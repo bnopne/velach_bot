7cf4b3c64105cb0f6d26318bb76b8e87bc105940
bnopne <bnopne@gmail.com>
2023-01-06 15:23:36 +0300

    Minor changes: read admin site port and host from config, disable admin site for now, minor improvements

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,26 @@
 import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 
 import { MainModule } from 'src/modules/main/main.module';
+import { ConfigurationService } from 'src/modules/configuration/configuration.service';
 
 config();
 
+const logger = new Logger('Bootstrap');
+
 async function bootstrap() {
   NestFactory.create(MainModule)
-    .then((app) => app.listen(3000))
+    .then((app) => {
+      const configurationService = app.get(ConfigurationService);
+
+      return app.listen(
+        configurationService.adminSitePort,
+        configurationService.adminSiteHost,
+      );
+    })
     .catch((err) => {
-      console.error(err);
+      logger.error(err);
     });
 }
 
