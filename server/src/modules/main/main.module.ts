24e0e97d2da88827c743785c19fe47daf86d81df
bnopne <bnopne@gmail.com>
2024-03-02 17:57:03 +0300

    WIP

--- server/src/modules/main/main.module.ts
+++ server/src/modules/main/main.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { TelegramBotModule } from 'src/modules/telegram-bot/telegram-bot.module';
 import { AdminApiModule } from 'src/modules/admin-api/admin-api.module';
 import { TasksModule } from 'src/modules/tasks/tasks.module';
+import { FilesModule } from 'src/modules/files/files.module';
 
 @Module({
-  imports: [AdminApiModule, TelegramBotModule, TasksModule],
+  imports: [AdminApiModule, TelegramBotModule, TasksModule, FilesModule],
   controllers: [],
   providers: [],
 })
---
+++ server/src/modules/telegram-bot/tasks/download-bikecheck.ts
@@ -0,0 +1,3 @@
+import { BaseTask } from 'src/modules/tasks/base-task';
+
+export class DownloadBikecheckPictureTask extends BaseTask {}
