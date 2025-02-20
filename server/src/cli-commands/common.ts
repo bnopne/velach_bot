58460eb917745b402c9c7b5e0bce45a32f317822
bnopne <bnopne@gmail.com>
2025-02-20 23:20:25 +0300

    Fixup commander usage

--- server/src/cli-commands/common.ts
+++ server/src/cli-commands/common.ts
@@ -1,13 +1,17 @@
 import { cwd } from 'node:process';
 import { join } from 'node:path';
 
+import { Logger } from '@nestjs/common';
+
 import { type ICliCommand } from './types';
+
+export const logger = new Logger('CLI');
 
 export async function execute(command: ICliCommand): Promise<void> {
   try {
     await command();
   } catch (err) {
-    console.error(err);
+    logger.error(err);
     process.exit(1);
   }
 }
--- server/src/cli.ts
+++ server/src/cli.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { createCommand } from 'commander';
 
-import { execute } from './cli-commands/common';
+import { execute, logger } from './cli-commands/common';
 import { type ICliCommand } from './cli-commands/types';
 import { applyMigrations } from './cli-commands/apply-migrations';
 import { createDbDump } from './cli-commands/create-db-dump';
@@ -14,36 +14,42 @@ import { cleanupDumpDirectory } from './cli-commands/cleanup-dump-directory';
 config();
 
 const program = createCommand()
-  .version('1.0.0')
+  .version('2.0.0')
   .option('--create-tables', 'Creates tables and corresponding stuff in DB')
   .option('--drop-tables', 'Drops tables and corresponding stuff in DB')
   .option('--seed-test-db', 'Fills DB with test data')
-  .option('--backup-db', 'Creates DB dump')
-  .option('--cleanup-dump-directory', 'Removes old DB dumps')
-  .option('--create-migration', 'Creates empty migration file')
+  .option('--backup-db <args...>', 'Creates DB dump')
+  .option('--cleanup-dump-directory <arg>', 'Removes old DB dumps')
+  .option('--create-migration <arg>', 'Creates empty migration file')
   .option('--apply-migrations', 'Applies all pending migrations')
   .parse(process.argv);
 
 let command: ICliCommand = () => Promise.reject('unknown CLI params');
 
-if (program.opts().createTables) {
+logger.log('parse CLI arguments');
+
+const options = program.opts();
+
+logger.log(JSON.stringify(options, null, 2));
+
+if (options.createTables) {
   console.debug('execute CREATE TABLES');
   command = createTables;
-} else if (program.opts().dropTables) {
+} else if (options.dropTables) {
   console.debug('execute DROP TABLES');
   command = dropTables;
-} else if (program.opts().seedTestDb) {
+} else if (options.seedTestDb) {
   console.debug('execute SEED TEST DATABASE');
   command = seedTestDb;
-} else if (program.opts().backupDb) {
+} else if (options.backupDb) {
   console.debug('execute BACKUP DATABASE');
-  command = () => createDbDump(program.args[0], program.args[1]);
-} else if (program.opts().cleanupDumpDirectory) {
-  command = () => cleanupDumpDirectory(program.args[0]);
-} else if (program.opts().createMigration) {
+  command = () => createDbDump(options.backupDb[0], options.backupDb[1]);
+} else if (options.cleanupDumpDirectory) {
+  command = () => cleanupDumpDirectory(options.cleanupDumpDirectory);
+} else if (options.createMigration) {
   console.debug('execute CREATE MIGRATION FILE');
-  command = () => createMigration(program.args[0]);
-} else if (program.opts().applyMigrations) {
+  command = () => createMigration(options.createMigration);
+} else if (options.applyMigrations) {
   console.debug('execute APPLY MIGRATIONS');
   command = applyMigrations;
 }
